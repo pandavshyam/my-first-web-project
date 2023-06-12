02d61723ce85a772e6a69865d2a76a261262901c
shyampandav <pandavshyam@gmail.com>
2023-06-13 00:32:06 +0530

    Add first JSP page

--- src/main/java/com/practice/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/practice/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -29,4 +29,15 @@ public class SayHelloController {
     sb.append("</html>");
     return sb.toString();
   }
+
+  //
+	// "say-hello-jsp" => sayHello.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+  // /src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
+  @RequestMapping("say-hello-jsp")
+  public String sayHelloJsp(){
+    return "sayHello";
+  }
 }
