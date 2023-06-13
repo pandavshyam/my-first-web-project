64e1f401b79708fd3d418ceb6b2440de9bb7f879
shyampandav <pandavshyam@gmail.com>
2023-06-13 23:38:14 +0530

    Add name and password support for login page

--- src/main/java/com/practice/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/practice/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,23 @@
 package com.practice.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-  @RequestMapping("login")
+  @RequestMapping(value = "login", method = RequestMethod.GET)
   public String goToLoginPage(){
     return "login";
+  }
+
+  @RequestMapping(value = "login", method = RequestMethod.POST)
+  public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
+    model.put("name", name);
+    model.put("password", password);
+    return "welcome";
   }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Welcome Page</title>
+  </head>
+  <body>
+    <div>Welcome to the website!</div>
+    <div>Your name is ${name}</div>
+    <div>Your password is ${password}</div>
+  </body>
+</html>
