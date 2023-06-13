6fc4600d0a5f0f8c3c499d78b21b5b1c00ccb985
shyampandav <pandavshyam@gmail.com>
2023-06-14 00:06:03 +0530

    Add authentication service

--- src/main/java/com/practice/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/practice/springboot/myfirstwebapp/login/LoginController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+  private AuthenticationService authenticationService;
+
+  public LoginController(AuthenticationService authenticationService){
+    super();
+    this.authenticationService = authenticationService;
+  }
 
   @RequestMapping(value = "login", method = RequestMethod.GET)
   public String goToLoginPage(){
@@ -16,8 +22,11 @@ public class LoginController {
 
   @RequestMapping(value = "login", method = RequestMethod.POST)
   public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
-    model.put("name", name);
-    model.put("password", password);
-    return "welcome";
+    if(authenticationService.authenticate(name, password)) {
+      model.put("name", name);
+      return "welcome";
+    }
+    model.put("errorMessage", "Invalid credentials! Please try again");
+    return "login";
   }
 }
