0e80b50100ea4b6ab843fbfbbaf4813324440497
shyampandav <pandavshyam@gmail.com>
2023-06-18 19:28:36 +0530

    Get username from spring security context

--- src/main/java/com/practice/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/practice/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,6 +3,8 @@ package com.practice.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -24,14 +26,15 @@ public class TodoController {
 
   @RequestMapping("list-todos")
   public String listAllTodos(ModelMap model){
-    List<Todo> todos = todoService.findByUsername("shyam");
+    String username = getLoggedInUsername();
+    List<Todo> todos = todoService.findByUsername(username);
     model.addAttribute("todos", todos);
     return "listTodos";
   }
 
   @RequestMapping(value = "add-todo", method = RequestMethod.GET)
   public String showNewTodoPage(ModelMap model) {
-    String username = (String)model.get("name");
+    String username = getLoggedInUsername();
     Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
     model.put("todo", todo);
     return "todo";
@@ -43,7 +46,7 @@ public class TodoController {
       return "todo";
     }
 
-    String username = (String)model.get("name");
+    String username = getLoggedInUsername();
     todoService.addTodo(username, todo.getDescription(),
       todo.getTargetDate(), false);
     return "redirect:list-todos";
@@ -68,9 +71,14 @@ public class TodoController {
       return "todo";
     }
 
-    String username = (String)model.get("name");
+    String username = getLoggedInUsername();
     todo.setUsername(username);
     todoService.updateTodo(todo);
     return "redirect:list-todos";
+  }
+
+  private String getLoggedInUsername(){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    return authentication.getName();
   }
 }
